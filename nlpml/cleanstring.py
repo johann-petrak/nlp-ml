97a9067f30e20b0f9cc4f6074fa75cb3a947f3c6
Johann Petrak <johann.petrak@gmail.com>
2019-02-28 14:32:51 +0100

    WIP

--- nlpml/cleanstring.py
+++ nlpml/cleanstring.py
@@ -9,25 +9,7 @@ import codecs
 import re
 import htmlparser
 import sys
-
-# Notes: 
-# replace hex codes which represent unicode, but replace invalid stuff with spaces:
-# codecs.decode(codecs.escape_decode(r"äà\xc3\xa4")[0], encoding="utf-8", errors='replace') 
-#   followed by replace all � with space
-# However this does NOT decode stuff like \n or \t
-# Also does not decode unicode escapes of the form \u00e4 
-# To decode unicode escapes from a string, we need to use codecs.decode(thestring, encoding="unicode-escape")
-# This also handles python string escapes like tab etc.
-# but this does not handle anything that is already unicode! So we can only apply it to the pattern itself!
-
-# Once we have a unicode string we should normalise to NFC or NFD using unicodedata.normalize("NFC", ustring)
-# We probably want "NFC"or "NFKC" here: both decompose then compose again but NFKC uses compatibility mode
-# so e.g. a ligature fi will get composed into separate fi characters which we probably want usually.
-
-# Possible strategy to do this:
-# 1) replace HTML character entities use html.unescape 
-# 2) use above strategy to replace hex escapes, this should leave unicode escapes and python string escapes intact
-# 3) then iterate over the unicode escapes and the python string escapes and replace each with the unicode-escape method
+import unicodedata
 
 RE_FIND_UESCAPES = re.compile(r'''\\U[0-9a-fA-F]{8,8}|\\u[0-9a-fA-F]{4,4}|\\x[0-9a-fA-F]{2,2}\\[0-7]{1,3}\\[\\'"abfnrtv]''', re.UNICODE)
 
@@ -48,6 +30,7 @@ def clean_string(thestring, replace_invalid=None, process_html=True, parse_html=
             parser.feed(thestring)
             parser.close()
             thestring = parser.text()
+            thestring = html.unescape(thestring)
         else:
             thestring = html.unescape(thestring)
     # step 2: remove hex escapes, and allow hex escapes sequences to get interpreted as UTF-8
@@ -65,5 +48,15 @@ def clean_string(thestring, replace_invalid=None, process_html=True, parse_html=
         thestring = thestring.replace("\ufffd", replace_invalid)
     # step 4: replace various kinds of unicode-escapes by searching and replacing just the specific occurrences of the escapes
     thestring = RE_FIND_UESCAPES.sub(lambda m: codecs.decode(m.group(0), 'unicode-escape'), thestring)
+    # step 5: normalize unicode to composed compatibility mode
+    thestring = unicodedata.normalize("NFKC", thestring)
     return thestring
 
+if __name__ == "__main__":
+    teststring = r"""Ligature ﬁ followed by html pound &pound; and HTML 969 for omega &#969; and ampersand &amp; <p>
+    Ligature again as HTML 64257: &#64257; and UTF8 hex codes \xef\xac\x81 and unicode 16 \ufb01<p>
+    a line with a <a href="asasa">link</a>
+    And here is a <div>div</div>
+    """
+    print("Testing cleanstring with: \n", teststring)
+    print(clean_string(clean_string(teststring)))
