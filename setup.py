0a90a8a67272b81c170f41868ff65d1a4be66615
Johann Petrak <johann.petrak@gmail.com>
2019-05-17 13:29:42 +0200

    Make autodiscovery of the unittests work.

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
 from setuptools import setup, find_packages
 
-setup(name="nlpml", packages=find_packages())
+def mytests():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
+    return test_suite
+
+setup(
+    name="nlpml", 
+    packages=find_packages(),
+    test_suite='setup.mytests'
+    )
 
