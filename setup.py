716082bbf728b52511cb6c578b03056ffb527b14
Johann Petrak <johann.petrak@gmail.com>
2019-09-17 16:14:44 +0200

    Improve setup, use pytest for testing

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-import unittest
+import os
 from setuptools import setup, find_packages
 
-def mytests():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover("tests", pattern="test_*.py")
-    return test_suite
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    readme = f.read()
 
 setup(
     name="nlpml", 
+    version="0.1",
+    author="Johann Petrak",
+    author_email="johann.petrak@gmail.com",
+    description="Library for opinionated approach to NLP, ML and Multiprocessing",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    setup_requires=["pytest-runner"],
+    install_requires=["torch>=1.0"],
+    python_requires=">=3.5",
+    tests_require=['pytest'],
+    platforms='any',
+    license="MIT",
+    keywords="",
+    url="http://github.com/johann-petrak/nlpml",
     packages=find_packages(),
-    test_suite='setup.mytests'
+    # test_suite='???'
     )
 
